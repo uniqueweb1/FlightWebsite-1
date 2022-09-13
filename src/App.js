1b25293ae44891410d2d83e2d96fe6d6245e6dad
oyinlola <70485142+uniqueweb1@users.noreply.github.com>
2022-09-13 02:14:52 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,61 +1,64 @@
-import React from "react";
-import AdminSettings from './components/AdminSettings.js';
-import SettingsTab from './components/SettingsTab.js';
-import Users from './components/Users.js';
+import React from 'react';
 import './App.css';
-import './style.css';
-// importing components....///
-import Sched from './components/Sched.js';
-import Sales from './components/Sales.js';
-import Ops from './components/Ops.js';
-import Crew from './components/Crew.js';
-import Handling from './components/Handling.js';
-import Mix from './components/Mix.js';
-import Report from './components/Report';
+import Navbar from './Components/Navbar/Navbar';
+import AdminSettings from './Pages/AdminSettings.js'
+import SettingsTab from './Pages/AdminSettingsPages/SettingsTab.js'
+import Users from './Pages/Users.js'
+import FullUserEdit from './Components/UserComponents/FullUserEdit.js'
+import Sched from './Pages/Sched';
+import Sales from './Pages/Sales';
+import Ops from './Pages/Ops';
+import Crew from './Pages/Crew';
+import Handling from './Pages/Handling';
+import Report from './Pages/Report';
 
+import{BrowserRouter as Router ,Routes, Route} from 'react-router-dom';
+import Fleet from './Pages/MX Pages/Fleet'
+import FleetDocument from './Pages/MX Pages/FleetDocument'
+import FleetDocumentForm from './Pages/MX Pages/FleetDocument'
+import FleetForm from './Pages/MX Pages/MX Pages Component/FleetForm';
+import FleetPlanning from'./Pages/MX Pages/FleetPlanning';
+import HoldItemList from './Pages/MX Pages/HoldItemList';
+import Home from'./Pages/Home';
+import LogOut from'./Pages/LogOut';
+import NewHIL from'./Pages/MX Pages/MX Pages Component/NewHIL';
+import NewscheduledMaintenance from'./Pages/MX Pages/MX Pages Component/NewscheduledMaintenance';
+import AddNewscheduledMaintenance from './Pages/MX Pages/MX Pages Component/AddNewscheduledMaintenanance';
+import ScheduledMaintenance from './Pages/MX Pages/ScheduledMaintenance';
+import Tlb from'./Pages/MX Pages/Tlb';
 
-import { BrowserRouter as Router , Route, Link, Routes } from 'react-router-dom';
-window.myUTC = 'mine';
+// route and link  to the pages element//
 function App() {
+
   return (
-    <div id="app">
-        <Router basename={process.env.PUBLIC_URL}> 
-          <header>
-            <nav>
-              <h1 id="logo">T<sub>r</sub><span className="logoS"></span></h1>
-              <div className="container-menu">
-                <Link to="/"><div id="menu-item">scheduling</div></Link>
-                <Link to="/sales"><div id="menu-item">sales<i className="fas fa-caret-down"></i></div></Link>
-                <Link to="/ops"><div id="menu-item">ops<i className="fas fa-caret-down"></i></div></Link>
-                <Link to="/crew"><div id="menu-item">crew<i className="fas fa-caret-down"></i></div></Link>
-                <Link to="/crew"><div id="menu-item">handling<i className="fas fa-caret-down"></i></div></Link>
-                <Link to="/mix"><div id="menu-item">mix<i className="fas fa-caret-down"></i></div></Link>
-                <Link to="/report"><div id="menu-item">report<i className="fas fa-caret-down"></i></div></Link> 
-              </div>
-              <div className="top__right-panel">
-                  <a href="/"><i id='icon' className="fa fa-plane"></i></a>
-                  <a href="/"><i id='icon' className="fas fa-wrench"></i></a>
-                  <Link to="/AdminSettings"><i id='icon' className="fa fa-cog"></i></Link>
-                  <a href="/"><i id='icon' className="fas fa-user"></i></a>
-              </div>
-              </nav>
-         
-            </header>
-            <Routes>
-              <Route path="/" element={<Sched />} />
-              <Route path="/sales" element={<Sales />} />
-              <Route path="/ops" element={<Ops />} />
-              <Route path="/crew" element={<Crew />} />
-              <Route path="/handling" element={<Handling />} />
-              <Route path="/mix" element={<Mix />} />
-              <Route path="/report" element={<Report />} /> 
-              <Route path="/AdminSettings" element={<AdminSettings />} /> 
-              <Route path="/url/to/SettingsTab" element={<SettingsTab />} />
-              <Route path="/url/to/Users" element={<Users />} />
-              {/* <Route path="*" element={<NoMatch />} /> */}
-            </Routes>       
-          </Router>
-    </div>
+   <Router>
+    <Navbar />
+    <Routes>
+<Route exact path='/'  element={<Home />} />
+<Route exact path='/fleetplanning' element={<FleetPlanning />} />
+<Route exact path='/fleet' element={<Fleet />} />
+<Route exact path='/fleetdocuments' element={<FleetDocument />} />
+<Route exact path='/scheduledmaintenance' element={<ScheduledMaintenance />} />
+<Route exact path='/holditemlist' element={<HoldItemList />} />
+<Route exact path='/tlb' element={<Tlb />} />
+<Route exact path='/log-out' element={<LogOut />} />
+<Route exact path='/add-new-scheduled-maintenance' element={<AddNewscheduledMaintenance />} />
+<Route exact path='/new-scheduled-maintenance' element={<NewscheduledMaintenance />} />
+<Route exact path='/new-hil' element={<NewHIL />} />
+<Route exact path='/fleetform' element={<FleetForm />} />
+<Route exact path='/fleet-document-form' element={<FleetDocumentForm />} />
+<Route exact path="/" element={<Sched />} />
+<Route exact path="/sales" element={<Sales />} />
+<Route exact path="/ops" element={<Ops />} />
+<Route exact path="/crew" element={<Crew />} />
+<Route exact path="/handling" element={<Handling />} />
+<Route exact  path="/report" element={<Report />} /> 
+<Route exact path="/admin-settings" element={<AdminSettings />} /> 
+<Route exact path="/url/to/SettingsTab" element={<SettingsTab />} />
+<Route exact path="/url/to/Users" element={<Users />} />
+<Route exact path="/url/to/FullUserEdit" element={<FullUserEdit />} />
+</Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,148 @@
+import React, { useState } from 'react'
+import Button from '../../Components/Button';
+import SyncIcon from '@mui/icons-material/Sync';
+import FlightIcon from '@mui/icons-material/Flight';
+import PersonIcon from '@mui/icons-material/Person';
+import SettingsIcon from '@mui/icons-material/Settings';
+import BuildIcon from '@mui/icons-material/Build';
+import {Link} from 'react-router-dom'
+import '../../Styles/Navbar.css';
+import Dropdown  from './Dropdown';
+
+function Navbar() {
+const [click, setClick] = useState(false);
+const [dropdown, setDropdown] = useState(false);
+
+const handleClick = () => setClick(!click);
+const closeMobileMenu = () => setClick(false);
+
+const onMouseEnter = () => {
+  if(window.innerWidth < 960) {
+    setDropdown(true);
+  } else{
+    setDropdown(true);
+  }
+};
+
+const onMouseLeave = () => {
+  if(window.innerWidth < 960) {
+    setDropdown(false);
+  } else{
+    setDropdown(false);
+  }
+};
+  return (
+    // nav section ul, li, link and hambuger menu //
+    <>
+        <nav className='navbar'>
+            
+            <ul className={click? 'nav-menu active' : 'nav-menu'}>
+              <li className='nav-item'>
+            <Link to='/' className='Navbar-logo'>
+            <h1 id="logo">T<sub>r</sub><span className="logoS"></span></h1>
+            </Link>
+            </li>
+<li className='nav-item'>
+<Link to='/scheduling' className='nav-links' onClick={closeMobileMenu}>
+  SCHEDULING
+</Link>
+ </li>
+  <li className='nav-item'>
+    <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+    FLIGHTS<i className='fas fa-caret-down' />
+    </Link>
+  </li>
+
+  <li className='nav-item'>
+    <Link to='/sales' className='nav-links' onClick={closeMobileMenu}>
+    SALES<i className='fas fa-caret-down' />
+    </Link>
+  </li>
+
+  <li className='nav-item'>
+    <Link to='/ops' className='nav-links' onClick={closeMobileMenu}>
+    OPS<i className='fas fa-caret-down' />
+    </Link>
+  </li>
+
+  <li className='nav-item'>
+    <Link to='/contact-us' className='nav-links' onClick={closeMobileMenu}>
+     HANDLING<i className='fas fa-caret-down' />
+    </Link>
+  </li>
+
+  <li className='nav-item' onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+
+    <p className='nav-links' onClick={closeMobileMenu}>
+MX <i className='fas fa-caret-down' />
+    </p>
+{dropdown && <Dropdown />}
+  </li>
+
+  <li className='nav-item'>
+    <Link to='/crew' className='nav-links' onClick={closeMobileMenu}>
+CREW<i className='fas fa-caret-down' />
+    </Link>
+  </li>
+
+  <li className='nav-item'>
+    <Link to='/report' className='nav-links' onClick={closeMobileMenu}>
+     REPORT<i className='fas fa-caret-down' />
+    </Link>
+  </li>
+  
+  
+</ul>
+
+            <ul className={click? 'nav-menu1 active' : 'nav-menu1'}>
+
+<li className='nav-item1'>
+  <Link to='/' className='nav-links1' onClick={closeMobileMenu}>
+  <FlightIcon></FlightIcon> 
+  </Link>
+</li>
+
+
+<li className='nav-item1'>
+
+  <Link to='/products' className='nav-links1' onClick={closeMobileMenu}>
+  <BuildIcon></BuildIcon><i className='fas fa-caret-down' />
+  </Link>
+</li>
+
+<li className='nav-item1'>
+  <Link to='/admin-settings' className='nav-links1' onClick={closeMobileMenu}>
+  <SettingsIcon></SettingsIcon> 
+  </Link>
+</li>
+
+
+<li className='nav-item1'>
+  <Link to='/user-settinngs' className='nav-links1' onClick={closeMobileMenu}>
+   <PersonIcon></PersonIcon>
+  </Link>
+</li>
+
+<li className='nav-item1'>
+  <Link to='/refresh' className='nav-links1' onClick={closeMobileMenu}>
+   <SyncIcon></SyncIcon> 
+  </Link>
+</li>
+<li className='nav-item1'>
+    <Link to='/log-out' className='nav-links-mobile' onClick={closeMobileMenu}>
+     Logout
+    </Link>
+  </li>
+</ul>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click? 'fas fa-times' : 'fas fa-bars'}></i>
+          </div>
+        
+         
+          <Button />
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
